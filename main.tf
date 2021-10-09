ed5a1238d79dbcc23a643c2bd0d9385e75ed733b
ssnyders89 <ssnyders@hotmail.com>
2021-10-09 21:21:57 +1100

    updated main.tf

--- main.tf
+++ main.tf
@@ -37,23 +37,27 @@ resource "azurerm_container_registry" "acr" {
   }
 }
 
-/*
+
 resource "azurerm_container_group" "challenge01" {
   name                = "techchallenge"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   ip_address_type     = "public"
-  dns_name_label      = "aci-label"
+  dns_name_label      = "servian-challenge"
   os_type             = "Linux"
-
+  image_registry_credential {
+    username = "contregchallenservian01"
+    password = data.azurerm_key_vault_secret.acrsecret.value
+    server = "contregchallenservian01.azurecr.io"
+  }
   container {
-    name   = "hello-world"
-    image  = "microsoft/aci-helloworld:latest"
+    name   = "challenge"
+    image  = "contregchallenservian01.azurecr.io/techchallengeapp:1.1"
     cpu    = "0.5"
     memory = "1.5"
 
     ports {
-      port     = 443
+      port     = 3000
       protocol = "TCP"
     }
   }
@@ -62,10 +66,10 @@ resource "azurerm_container_group" "challenge01" {
     environment = var.environment
   }
 }
-*/
+
 
 resource "azurerm_key_vault" "challenge" {
-  name                        = "challenge"
+  name                        = "challenge-pwd"
   location                    = azurerm_resource_group.this.location
   resource_group_name         = azurerm_resource_group.this.name
   enabled_for_disk_encryption = true
@@ -90,12 +94,34 @@ resource "azurerm_key_vault" "challenge" {
     storage_permissions = [
       "Get",
     ]
-  }
+    }
+
+}
+
+  resource "azurerm_key_vault_secret" "challenge1" {
+  name         = "postgressqlsecret"
+  value        = random_string.postgressqlsecret.result
+  key_vault_id = azurerm_key_vault.challenge.id
+
     tags = {
     environment = var.environment
   }
 }
 
+  resource "azurerm_key_vault_secret" "acrsecret" {
+  name         = "acrsecret"
+  value        = azurerm_container_registry.acr.admin_password
+  key_vault_id = azurerm_key_vault.challenge.id
+
+    tags = {
+    environment = var.environment
+  }
+}
+resource "random_string" "postgressqlsecret" {
+  length           = 16
+  special          = false
+  override_special = "/@£$"
+}
 
 resource "azurerm_postgresql_server" "challenge" {
   name                = "postgresql-challenge-1"
@@ -110,9 +136,9 @@ resource "azurerm_postgresql_server" "challenge" {
   auto_grow_enabled            = true
 
   administrator_login          = "psqladminun"
-  administrator_login_password = "H@Sh1CoR3!"
+  administrator_login_password = data.azurerm_key_vault_secret.challenge.value
   version                      = "9.6"
-  ssl_enforcement_enabled      = true
+  ssl_enforcement_enabled      = false
     tags = {
     environment = var.environment
   }
@@ -136,4 +162,16 @@ resource "azurerm_storage_account" "challenge" {
   tags = {
     environment = var.environment
   }
+}
+
+
+
+data "azurerm_key_vault_secret" "challenge" {
+  name         = "postgressqlsecret"
+  key_vault_id = azurerm_key_vault.challenge.id
+}
+
+data "azurerm_key_vault_secret" "acrsecret" {
+  name         = "acrsecret"
+  key_vault_id = azurerm_key_vault.challenge.id
 }
